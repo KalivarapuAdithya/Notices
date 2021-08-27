7d2c8fb1de6fe581b90cbe4ba2467a0370585f6f
KalivarapuAdithya <adithyakalivarapu@gmail.com>
2021-08-27 16:31:18 +0530

    active

--- app.js
+++ app.js
@@ -233,7 +233,6 @@ app.listen(PORT , ()=>{
     wakeDyno({
         url: "https://notices0136.herokuapp.com/" ,  // url string
         interval: 60000, // interval in milliseconds (1 minute in this example)
-        startNap: [0, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
-        endNap: [5, 59, 59, 999] // time to wake up again, in UTC (09:59:59.999 in this example)
+        
     }).start(); 
 })
